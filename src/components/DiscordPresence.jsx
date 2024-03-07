1fdf23509ffa52c7e9aa7213e480bb959ffb5bd7
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-07 00:16:43 -0300

    feat: add details and state to tooltip

--- src/components/DiscordPresence.jsx
+++ src/components/DiscordPresence.jsx
@@ -15,7 +15,7 @@ const DiscordPresence = () => {
     offline: "Desconectado",
   };
 
-
+  const gameActivity = status && status.activities ? status.activities.find((ac) => ac.type === 0) : null;
   return (
     <>
       {!loading && statusMap[status.discord_status] ? (
@@ -28,14 +28,20 @@ const DiscordPresence = () => {
               </span>
               <Tooltip id="discord-id" />
             </button>
-            {status.activities.find((ac) => ac.type === 0)&& (
+            {gameActivity && (
               <div>
-                <button target="_blank" data-tooltip-id="game-playing" data-tooltip-content={`Jugando: ${status.activities.find((ac) => ac.type === 0).name}\n${status.activities.find((ac) => ac.type === 0).details ?? ""}`} data-tooltip-place="bottom" className="spotify-button select-none rounded-xl py-2 px-3 gap-2 items-center inline-flex text-gray-300 bg-neutral-800 mt-3">
+                <button target="_blank" data-tooltip-id="game-playing" data-tooltip-place="bottom" className="spotify-button select-none rounded-xl py-2 px-3 gap-2 items-center inline-flex text-gray-300 bg-neutral-800 mt-3">
                   <img src="joystick.svg" alt="" className={`status-dot`}></img>
                   <span className="m-0 mr-1 p-0 text-center text-md select-none md:h-5">
-                    {status.activities.find((ac) => ac.type === 0).name.length < 25 ? status.activities.find((ac) => ac.type === 0).name : ((status.activities.find((ac) => ac.type === 0).name.slice(0, 25) + "..." ))}
+                    {gameActivity.name.length < 25 ? gameActivity.name : gameActivity.name.slice(0, 25) + "..." }
                   </span>
-                  <Tooltip id="game-playing" />
+                  <Tooltip id="game-playing">
+                    <div>
+                      <h3>Jugando: {gameActivity.name}</h3>
+                      <p>{gameActivity.details}</p>
+                      <p>{gameActivity.state}</p>
+                    </div>
+                  </Tooltip>
                 </button>
               </div>
             )}
