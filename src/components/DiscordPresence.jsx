9f2f61a57498b891eeb349c511978fe97babfac5
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-12 14:29:33 -0300

    feat: add hover effect to all pills

--- src/components/DiscordPresence.jsx
+++ src/components/DiscordPresence.jsx
@@ -21,7 +21,7 @@ const DiscordPresence = () => {
       {!loading && statusMap[status.discord_status] ? (
         <>
           <div className="md:flex gap-2">
-            <button data-tooltip-id="discord-id" data-tooltip-content="Discord: misfitdude" data-tooltip-place="bottom" className="status-button select-none rounded-xl py-2 px-3 gap-2 items-center inline-flex text-gray-300 bg-neutral-800 max-w-[300px] mt-3">
+            <button data-tooltip-id="discord-id" data-tooltip-content="Discord: misfitdude" data-tooltip-place="bottom" className="status-button select-none rounded-xl py-2 px-3 gap-2 items-center inline-flex text-gray-300 bg-neutral-800 hover:bg-neutral-700 transition-colors duration-50 ease-in-out max-w-[300px] mt-3">
               <span className={`status-dot ${status.discord_status}`}></span>
               <span className="m-0 mr-1 p-0 text-center text-md select-none h-5">
                 {!loading && statusMap[status.discord_status]}
@@ -30,7 +30,7 @@ const DiscordPresence = () => {
             </button>
             {gameActivity && (
               <div>
-                <button target="_blank" data-tooltip-id="game-playing" data-tooltip-place="bottom" className="spotify-button select-none rounded-xl py-2 px-3 gap-2 items-center inline-flex text-gray-300 bg-neutral-800 mt-3">
+                <button target="_blank" data-tooltip-id="game-playing" data-tooltip-place="bottom" className="spotify-button select-none rounded-xl py-2 px-3 gap-2 items-center inline-flex text-gray-300 bg-neutral-800 hover:bg-neutral-700 transition-colors duration-50 ease-in-out mt-3">
                   <img src="joystick.svg" alt="" className={`status-dot`}></img>
                   <span className="m-0 mr-1 p-0 text-center text-md select-none md:h-5">
                     {gameActivity.name.length < 25 ? gameActivity.name : gameActivity.name.slice(0, 25) + "..." }
@@ -49,7 +49,7 @@ const DiscordPresence = () => {
 
           {status.spotify && (
             <div>
-              <a target="_blank" data-tooltip-id="spotify-song" data-tooltip-content={`Escuchando en Spotify: ${status.spotify.artist} - ${status.spotify.song}`} data-tooltip-place="bottom" href={`https://open.spotify.com/track/${status.spotify.track_id}`} className="spotify-button select-none rounded-xl py-2 px-3 gap-2 items-center inline-flex text-gray-300 bg-neutral-800 mt-3">
+              <a target="_blank" data-tooltip-id="spotify-song" data-tooltip-content={`Escuchando en Spotify: ${status.spotify.artist} - ${status.spotify.song}`} data-tooltip-place="bottom" href={`https://open.spotify.com/track/${status.spotify.track_id}`} className="spotify-button select-none rounded-xl py-2 px-3 gap-2 items-center inline-flex text-gray-300 bg-neutral-800 hover:bg-neutral-700 transition-colors duration-50 ease-in-out mt-3">
                 <img src="spotify.svg" alt="" className={`status-dot`}></img>
                 <span className="m-0 mr-1 p-0 text-center text-md select-none md:h-5">
                   {status.spotify.artist.length < 25 ? status.spotify.artist : (status.spotify.artist.slice(0, 25) + '...')} - {status.spotify.song.length < 25 ? status.spotify.song : (status.spotify.song.slice(0, 25) + '...')}
