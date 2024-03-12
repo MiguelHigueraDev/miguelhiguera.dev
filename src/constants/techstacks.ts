36fa5585e3b0926c8d65a14427557bba640efcb1
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-03-12 15:56:00 -0300

    feat: update techstack styling

---
+++ src/constants/techstacks.ts
@@ -0,0 +1,57 @@
+export const frontend = [
+    {
+        name: "JavaScript",
+        icon: "javascript.svg"
+    },
+    {
+        name: "TypeScript",
+        icon: "typescript.svg"
+    },
+    {
+        name: "HTML",
+        icon: "html.svg"
+    },
+    {
+        name: "CSS",
+        icon: "css.svg"
+    },
+    {
+        name: "jQuery",
+        icon: "jquery.svg"
+    },
+    {
+        name: "Vue",
+        icon: "vue.svg"
+    },
+    {
+        name: "React",
+        icon: "react.svg"
+    },
+]
+
+export const backend = [
+    {
+        name: "PHP",
+        icon: "php.svg"
+    },
+    {
+        name: "Laravel",
+        icon: "laravel.svg"
+    },
+    {
+        name: "Java",
+        icon: "java.svg"
+    },
+    {
+        name: "Node",
+        icon: "node.svg"
+    },
+    {
+        name: "MySQL",
+        icon: "mysql.svg"
+    },
+    {
+        name: "PostgresSQL",
+        icon: "postgresql.svg"
+    },
+]
